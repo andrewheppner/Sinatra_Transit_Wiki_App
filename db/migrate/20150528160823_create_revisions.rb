a9cb1710444f2047a0102b86da9b19cb102aeb62
andrewheppner <andrewbheppner@gmail.com>
2015-05-28 16:23:53 +0000

    Change column names for edit dates in revisions

--- db/migrate/20150528160823_create_revisions.rb
+++ db/migrate/20150528160823_create_revisions.rb
@@ -4,8 +4,8 @@ class CreateRevisions < ActiveRecord::Migration
       t.belongs_to :users
       t.belongs_to :transit_modes
       t.string :column_name
-      t.datetime :before_created_at
-      t.datetime :after_created_at
+      t.datetime :before_edit
+      t.datetime :after_edit
       t.timestamps null:false 
     end
   end
