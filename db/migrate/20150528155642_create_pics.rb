6953b391831b3602ccdfd9f54989f177bf23b7d4
Bruno Opsenica <bruno.opsenica@gmail.com>
2015-05-28 12:49:07 -0400

    fix error in create_pics

--- db/migrate/20150528155642_create_pics.rb
+++ db/migrate/20150528155642_create_pics.rb
@@ -2,7 +2,7 @@ class CreatePics < ActiveRecord::Migration
   def change
     create_table :pics do |t|
       t.belongs_to :cities
-      t.belong_to :users
+      t.belongs_to :users
       t.string :path
     end
   end
