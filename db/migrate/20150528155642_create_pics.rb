7e3da78e805ee1eed922accfa2d6f851e7e72665
LifeofCode <LifeofCode@users.noreply.github.com>
2015-05-28 16:09:29 +0000

    fix pics migration

--- db/migrate/20150528155642_create_pics.rb
+++ db/migrate/20150528155642_create_pics.rb
@@ -1,6 +1,6 @@
 class CreatePics < ActiveRecord::Migration
   def change
-    create_table :pics do 
+    create_table :pics do |t|
       t.belongs_to :cities
       t.belong_to :users
       t.string :path
