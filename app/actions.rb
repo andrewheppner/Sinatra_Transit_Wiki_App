c1020cc7a21ecdcb954db65ac86bb52d378f7a1e
Bruno Opsenica <bruno.opsenica@gmail.com>
2015-05-28 15:01:32 -0400

    Rename font folder and modify registration route

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,13 @@ get '/register' do
   @user = User.new
   erb :'register'
 end
+
+post '/register' do
+  @user = User.new params[0..-2]
+  @user.errors.add[:password, "fields must match!"] if params[:password] != params[:password_confirm]
+  if @user.save
+    redirect '/register'
+  else
+    erb :'/register'
+  end
+end
