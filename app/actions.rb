b22afd01a010ca0a2bd6dcd47a7e0b78eec76f0a
Bruno Opsenica <bruno.opsenica@gmail.com>
2015-05-28 16:39:56 -0400

    fix syntax error in actions.rb, refactor to make more RESTful

--- app/actions.rb
+++ app/actions.rb
@@ -4,13 +4,14 @@ set sessions: true
 
 get '/' do 
   erb :index
-
-get '/register' do
-  @user = User.new
-  erb :'register'
 end
 
-post '/register' do
+get '/users/new' do
+  @user = User.new
+  erb :'/users/new'
+end
+
+post '/user' do
   @user = User.new(
     username: params[:username],
     email: params[:email],
@@ -18,9 +19,9 @@ post '/register' do
     )
   @user.errors.add[:password, "fields must match!"] if params[:password] != params[:password_confirm]
   if @user.save
-    redirect '/register'
+    redirect '/'
   else
-    erb :'/register'
+    erb :'/users/new'
   end
-  
+
 end
