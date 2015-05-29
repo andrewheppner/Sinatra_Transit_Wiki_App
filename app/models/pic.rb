959299917e995d98203572a1fea1e88db047ff19
LifeofCode <LifeofCode@users.noreply.github.com>
2015-05-29 20:05:03 +0000

    add default errors for pic/new

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -3,5 +3,5 @@ class Pic < ActiveRecord::Base
   belongs_to :city
 
   validates :path, presence: true 
-  validates :pic_title, presence: true, length: {within: 1..30}
+  validates :pic_title, presence: true, length: {within: 3..30}
 end
